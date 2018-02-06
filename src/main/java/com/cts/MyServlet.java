6ca15ad19776cd7e6fd67b55cd1b95185094e863
ganesanns <ganesan.ns@cognizant.com>
2018-02-06 15:06:40 +0530

    Resolve compilation issue

--- src/main/java/com/cts/MyServlet.java
+++ src/main/java/com/cts/MyServlet.java
@@ -1,10 +1,14 @@
+
+package com.cts;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class MyServlet extends HttpServlet {
+public class
+MyServlet extends HttpServlet {
 
     private static final long serialVersionUID = -4751096228274971485L;
 
